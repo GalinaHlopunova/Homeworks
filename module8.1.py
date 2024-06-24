089496a7e7740c9ac8300a0a3f6eafff8c66fe4c
GalinaHlopunova <166922606+GalinaHlopunova@users.noreply.github.com>
2024-06-24 13:22:13 +0300

    Add files via upload

--- module8.1.py
+++ module8.1.py
@@ -1,19 +1,18 @@
 def add_everything_up(a, b):
+    s = 0
     try:
         s = a + b
         if type(s) == float:
             s = float('{:.3f}'.format(s))
-        print(s)
     except TypeError:
         a = str(a)
         b = str(b)
         s = a + b
-        print(s)
+    return s
 
 
-
-add_everything_up(123.456, "строка")
-add_everything_up("яблоко", 4215)
-add_everything_up("фрукт ", "яблоко")
-add_everything_up(123.456, 7)
-add_everything_up(12, 23)
+print(add_everything_up(123.456, "строка"))
+print(add_everything_up("яблоко", 4215))
+print(add_everything_up("фрукт ", "яблоко"))
+print(add_everything_up(123.456, 7))
+print(add_everything_up(12, 23))
