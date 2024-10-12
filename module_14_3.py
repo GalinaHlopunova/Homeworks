137df94a95a8ad0a5380add531abe8dcdb191884
GalinaHlopunova <166922606+GalinaHlopunova@users.noreply.github.com>
2024-10-12 09:29:51 +0300

    Add files via upload

--- module_14_3.py
+++ module_14_3.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7652634238:AAEf1a-DiEYi-yFt5JsxckREselK2I4iJa0"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
